a31eebaccf95c5702607e5095effe5249ad363ba
F772 <lauris.gum@gmail.com>
2025-02-24 09:23:57 +0200

    Sukurti modeliai, admin laukai ir pagrindinis base html šablonas.

--- moviereviews/admin.py
+++ moviereviews/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Movie, Genre, Director, Review, Comment, Reaction
+from django.utils.html import format_html
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'director', 'image_preview')  # Pridėta nuotraukų peržiūra
+    search_fields = ('title', 'director__name')
+    list_filter = ('year', 'genres')
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" style="border-radius: 5px;" />', obj.image.url)
+        return "Nėra nuotraukos"
+
+    image_preview.short_description = 'Nuotrauka'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'movie', 'rating')
+    list_filter = ('rating',)
+    search_fields = ('title', 'movie__title')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'review')
+
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    list_display = ('reaction_type', 'review')
+
+
+
--- moviereviews/models.py
+++ moviereviews/models.py
@@ -20,10 +20,15 @@ class Director(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    release_year = models.PositiveIntegerField()
-    genres = models.ManyToManyField(Genre)
+    year = models.IntegerField()
+    genres = models.ManyToManyField(Genre, blank=True)
     director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True, blank=True)
     imdb_id = models.CharField(max_length=20, blank=True, null=True, unique=True)
+    image = models.ImageField(upload_to='movie_images/', blank=True, null=True)
+
+    def display_genres(self):
+        res = ', '.join(elem.name for elem in self.genres.all()[:3])
+        return res
 
     def __str__(self):
         return self.title
@@ -43,22 +48,29 @@ class Review(models.Model):
 
 
 class Comment(models.Model):
+    review = models.ForeignKey('Review', related_name='comments', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    review = models.ForeignKey(Review, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.user.username}"
+        return f"Comment by {self.user.username} on {self.review.title}"
 
 
 class Reaction(models.Model):
+    LIKE = 'like'
+    DISLIKE = 'dislike'
+    REACTION_CHOICES = [
+        (LIKE, 'Like'),
+        (DISLIKE, 'Dislike'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='reactions')
     reaction_type = models.CharField(max_length=10, choices=[('like', 'Like'), ('dislike', 'Dislike')])
 
     class Meta:
         unique_together = ('user', 'review')
 
     def __str__(self):
-        return f"{self.user.username} {self.reaction_type} {self.review.title}"
+        return f"{self.user.username} {self.reaction_type} {self.review}"
